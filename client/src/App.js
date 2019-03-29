591e0eb9ee2e27969b05534e09ff4fb73f7c0183
marian <rahman.marian@gmail.com>
2019-03-28 21:47:55 -0400

    Fixed home components.

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Home} from "./Home";
+import Home  from "./Home";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App--Aside"></div>
-          <div className="App--Form"></div>
-            <div className="PageSwitcher"></div>
-              <a href="#"className="PageSwitcher--Item">Sign In</a>
-              <a href="#" className="PageSwitcher--Item--PageSwitcher--Active">Sign Up</a>
-              </div>
-
-        <div className="FormTitle">
-          <a href="#" className="FormTitle--link"> Sign In</a> or <a href="#"className= "FormTitle--Link FormTitle--Link=Active"> Sign Up</a>
-        
-      
+        <Home />
       </div>
+
+
+
+
     )
   }
 }
 
 
-<div className="FormCenter">
 
-  <form className="FormFields"
-
-</div>
 export default App;
