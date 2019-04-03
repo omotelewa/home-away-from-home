f5216622d21e2d2cf3df63de618e080226ccc493
marian <rahman.marian@gmail.com>
2019-04-03 19:22:52 -0400

    deleted reservationsForm.js and modified Beach.js and Snow.js.

--- client/src/pages/Beach/Beach.js
+++ client/src/pages/Beach/Beach.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./Beach.css";
-import ReservationsForm from "../../components/Reservations/reservationsForm";
+// import ReservationsForm from "../../components/Reservations/reservationsForm";
 
 const Beach = () => (
   <div className="beachDiv">
       <div className="container">
         <h1 className="header">Beach Houses</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam, corrupti quo repellendus alias deleniti nostrum accusamus nihil a! Non eos, quidem sequi necessitatibus eius nobis, excepturi unde repellendus quisquam odit aut dolorum ut assumenda? Fuga sint quae saepe cum quas minima aut numquam. Illum, recusandae beatae assumenda velit voluptas alias?</p>
+        <p>Our Beach Houses are perfect for weekend or summer getaway!  We have individual challets that sleeps up to 10 people.  You can bring your family and your kids friends up to the Beach Houses in beautiful, South Carolina during our open season. </p>
         </div>
         <div>
-          <ReservationsForm/>
+          {/* <ReservationsForm/> */}
         </div>
   </div>
 
