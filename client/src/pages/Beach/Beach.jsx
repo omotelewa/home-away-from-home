9676f44127dfb59f91f958aa29043d7961456ba8
Rachel Mahan <rachelm2490@gmail.com>
2019-04-03 20:52:08 -0400

    Added departure datepicker, styles

--- client/src/pages/Beach/Beach.jsx
+++ client/src/pages/Beach/Beach.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import "./Beach.css";
+import { Form, Button } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "../../../node_modules/react-datepicker/src/stylesheets/datepicker-cssmodules.scss";
 
 class Beach extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      arrivalDate: "",
+      departureDate: ""
+    };
+  }
+
+  handleArrivalDateChange = date => {
+    this.setState({
+      arrivalDate: date
+    });
+  };
+
+  handleDepartureDateChange = date => {
+    this.setState({
+      departureDate: date
+    });
+  };
+
   render() {
     return (
       <div className="beachDiv">
@@ -18,7 +40,27 @@ class Beach extends React.Component {
             beatae assumenda velit voluptas alias?
           </p>
         </div>
-        <DatePicker selected={null} onChange={this.handleChange} />
+        <div className="res-form">
+          <Form>
+            <Form.Group controlId="arrival">
+              <Form.Label>Arrival Date</Form.Label>
+              <DatePicker
+                selected={this.state.arrivalDate}
+                onChange={this.handleArrivalDateChange}
+              />
+            </Form.Group>
+            <Form.Group controlId="departure">
+              <Form.Label>Departure Date</Form.Label>
+              <DatePicker
+                selected={this.state.departureDate}
+                onChange={this.handleDepartureDateChange}
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Reserve my spot
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
